3efd3595d51447e10bfad4a2d7667b74fc9a6d16
Inna <innascherbak11@gmail.com>
2017-02-03 16:08:00 +0200

    Improve Home page and add Users controller

--- Gemfile
+++ Gemfile
@@ -3,9 +3,11 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.8'
+gem 'bootstrap-sass'
+
 # Use sqlite3 as the database for Active Record
 #gem 'pg'
-gem 'pg'
+gem 'pg', '0.16.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'users/new'
 
-  get 'static_pages/about'
+  root 'static_pages#home'
+
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/signup', to: 'users#new', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
