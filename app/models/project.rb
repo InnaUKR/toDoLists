a7f8d52b340c9ab3be446d930af3048e258d55d6
innaUKR <innascherbak11@gmail.com>
2017-07-29 23:24:12 +0300

    Add task with AJAX

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
 	belongs_to :user
 	has_many :tasks, dependent: :destroy
-	default_scope -> { order('created_at DESC')}                                                                          
+	default_scope -> { order('created_at')}                                                                          
 	validates :user_id, presence: true
 	validates :title, presence: true
 	accepts_nested_attributes_for :tasks
